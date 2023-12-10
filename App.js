0e4e17860bb15eaa1f4c2e717b5d265eb5ef3f72
noeliamorato <moratocaeronoelia123@gmail.com>
2023-12-10 18:29:55 -0400

    screens: adiestramiento

--- App.js
+++ App.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './screens/Login';
-import MapScreen from './screens/MapScreen'; // Asegúrate de tener esta pantalla
-import GenerarQR from './screens/GenerarQR';
-import Welcome from './screens/Welcome';
-import Adiestramiento from './src/screens/Adiestramiento';
-import Agenda from './src/screens/Agenda';
-import Cuidar from './src/screens/Cuidar';
-import Inicio from './src/screens/Inicio';
-import Mapas from './src/screens/Mapas';
-import MisMascotas from './src/screens/MisMascotas';
-import Profesionales from './src/screens/Profesionales';
-import Salud from './src/screens/Salud';
-import ModalAgregarMascota from './src/screens/Inicio/ModalAgregarMascota';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/Login";
+import MapScreen from "./screens/MapScreen"; // Asegúrate de tener esta pantalla
+import GenerarQR from "./screens/GenerarQR";
+import Welcome from "./screens/Welcome";
+import Adiestramiento from "./src/screens/Adiestramiento";
+import Agenda from "./src/screens/Agenda";
+import Cuidar from "./src/screens/Cuidar";
+import Inicio from "./src/screens/Inicio";
+import Mapas from "./src/screens/Mapas";
+import MisMascotas from "./src/screens/MisMascotas";
+import Profesionales from "./src/screens/Profesionales";
+import Salud from "./src/screens/Salud";
+import ModalAgregarMascota from "./src/screens/Inicio/ModalAgregarMascota";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="MapScreen" component={MapScreen} />
-        <Stack.Screen name="GenerarQR" component={GenerarQR} />
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Adiestramiento" component={Adiestramiento} />
-        <Stack.Screen name="Agenda" component={Agenda} />
-        <Stack.Screen name="Cuidar" component={Cuidar} />
-        <Stack.Screen name="Inicio" component={Inicio} />
-        <Stack.Screen name="Mapas" component={Mapas} />
-        <Stack.Screen name="MisMascotas" component={MisMascotas} />
-        <Stack.Screen name="Profesionales" component={Profesionales} />
-        <Stack.Screen name="Salud" component={Salud} />
-        <Stack.Screen name="ModalAgregarMascota" component={ModalAgregarMascota} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="MapScreen" component={MapScreen} />
+          <Stack.Screen name="GenerarQR" component={GenerarQR} />
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Adiestramiento" component={Adiestramiento} />
+          <Stack.Screen name="Agenda" component={Agenda} />
+          <Stack.Screen name="Cuidar" component={Cuidar} />
+          <Stack.Screen name="Inicio" component={Inicio} />
+          <Stack.Screen name="Mapas" component={Mapas} />
+          <Stack.Screen name="MisMascotas" component={MisMascotas} />
+          <Stack.Screen name="Profesionales" component={Profesionales} />
+          <Stack.Screen name="Salud" component={Salud} />
+          <Stack.Screen
+            name="ModalAgregarMascota"
+            component={ModalAgregarMascota}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
--- src/screens/MisMascotas/index.js
+++ src/screens/MisMascotas/index.js
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, FlatList, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const MisMascotas = () => {
-  return (
-    <View>
-      <Text>MisMascotas</Text>
-    </View>
-  )
-}
+  const [searchText, setSearchText] = useState('');
+  const [mascotas] = useState([
+    { id: 1, nombre: 'Luna' },
+    { id: 2, nombre: 'Max' },
+    // Agrega más objetos con los datos de tus mascotas
+  ]);
 
-export default MisMascotas
+  // Filtrar mascotas según el texto de búsqueda
+  const filteredMascotas = mascotas.filter(mascota =>
+    mascota.nombre.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <Ionicons name="search" size={20} color="#888" style={styles.searchIcon} />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar mascota"
+          value={searchText}
+          onChangeText={text => setSearchText(text)}
+        />
+      </View>
+      <Text style={styles.registradasText}>Mascotas Registradas</Text>
+      <FlatList
+        data={filteredMascotas}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.mascotaItem}>
+            <Text>{item.nombre}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  searchInput: {
+    flex: 1,
+    paddingVertical: 10,
+    color: '#000', // Cambiar color del texto si es necesario
+  },
+  registradasText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  mascotaItem: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
+export default MisMascotas;
